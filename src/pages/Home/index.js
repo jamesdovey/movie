dce9e82578febbc076713b0c8ca7e8bc12857562
James Dovey <hi@jamesdovey.com>
2020-05-01 13:19:29 +0100

    Search bar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 
-import { Heading, Flex } from "@chakra-ui/core";
+import {
+  Heading,
+  Flex,
+  Input,
+  InputGroup,
+  Icon,
+  InputRightElement
+} from "@chakra-ui/core";
 
 const Home = () => {
   return (
-    <Flex>
-      <Heading>Hello</Heading>
+    <Flex justify="center" align="center" direction="column">
+      <Flex width="60%" justify="center" align="center" direction="column">
+        <Heading textAlign="center">Movie Search</Heading>
+        <InputGroup width="50%">
+          <Input placeholder="Search" size="lg" />
+          <InputRightElement children={<Icon name="search" color="black" />} />
+        </InputGroup>
+      </Flex>
     </Flex>
   );
 };
